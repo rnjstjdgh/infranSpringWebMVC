dcd9a54d6db86de61bd47d2ced316a8d978f5eb7
unknown <gshgsh0831@fasoo.com>
2021-06-16 16:59:16 +0900

    Finished Refactoring V2

---
+++ src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -0,0 +1,14 @@
+package hello.servlet.web.frontcontroller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelView {
+
+    private String viewName;
+    private Map<String, Object> model = new HashMap<>();
+
+
+
+
+}
